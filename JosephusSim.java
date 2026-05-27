1e8da17a9e9eb5be1e7dafc666952462a29a4237
JesseH0630 <zxh0630@outlook.com>
2026-05-27 11:24:08 -0700

    Debug for add method.

--- JosephusSim.java
+++ JosephusSim.java
@@ -39,11 +39,16 @@ public class JosephusSim {
 
 	// optional helper method for constructing the circle
 	private void add(String val) {
-		track = circle;
-		while(track.next != null) {
-			track = track.next;
+		if(circle == null) {
+			circle = new PersonNode(val);
 		}
-		track.next = new PersonNode(val);
+		else {
+			track = circle;
+			while(track.next != null) {
+				track = track.next;
+			}
+			track.next = new PersonNode(val);
+		}
 	}
 
 	public void eliminate() {
